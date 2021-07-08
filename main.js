01a1a045954fb30e04a2e3840520edd9bf8c0030
GRobin8889 <80229056+GRobin8889@users.noreply.github.com>
2021-07-07 21:14:13 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,32 @@
 'use strict';
 
+///////// Send Us An Email" Button Modal Functionality ////////////
+// Get the location Modal
+var modal = document.getElementById("email-modal");
+
+// Get the button that will open the location modal
+var btn = document.getElementById("cta-send")
+
+// Get the <span> element that will close the location Modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the location modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the location modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 //------------------Index.html JavaScript------------------//
 //---Contact Us Section---//
 ///////// Automatically Highlight Day of the Week in 'in-touch-section' ////////////
