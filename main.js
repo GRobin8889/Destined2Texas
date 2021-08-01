83f54417a4a52ebabf9047a0f2fc35b2168adfa8
GRobin8889 <80229056+GRobin8889@users.noreply.github.com>
2021-08-01 13:07:24 -0500

    Update main.js

--- main.js
+++ main.js
@@ -41,3 +41,51 @@ weekday[5] = "fri";
 weekday[6] = "sat";
 
 document.getElementsByClassName(weekday[day.getDay()])[0].classList.add("current-day");
+
+
+//----------------Listings Pages (attractions.html, food.html, lodging.html) Google Map JavaScript----------------//
+var marker = new google.maps.Marker({
+  position: myCenter
+});
+
+marker.setMap(map);
+
+// Map for the Dallas, Texas Hotels listed (lodging.html)
+function initMap() {
+  var dallasHotelLocations = [
+    ['Lorenzo Hotel Dallas', 32.7726198, -96.7978941, 1],
+    ['The Statler Dallas Curio Collection By Hilton', 32.7805914, -96.7970604, 2],
+    ['Magnolia Hotel Dallas Downtown', 32.7800339, -96.8014286, 3],
+    ['Sandman Signature Plano - Frisco Hotel', 33.0905206, -96.8171087, 4],
+    ['The Joule', 32.7805962, -96.8006609, 5]
+  ];
+
+  var center = {
+    lat: 32.776665,
+    lng: -96.796989
+  };
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10,
+    center: center,
+    MapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+
+  var infowindow = new google.maps.InfoWindow({});
+
+  var marker, i;
+
+  for (i = 0; i < dallasHotelLocations.length; i++) {
+    marker = new google.maps.Marker({
+      position: new google.maps.LatLng(dallasHotelLocations[i][1], dallasHotelLocations[i][2], dallasHotelLocations[i][3], dallasHotelLocations[i][4], dallasHotelLocations[i][5]),
+      map: map,
+    });
+
+    google.maps.event.addListener(marker, 'click', (function(marker, i) {
+      return function() {
+        infowindow.setContent(dallasHotelLocations[i][0]);
+        infowindow.open(map, marker);
+      }
+    })(marker, i));
+  }
+}
